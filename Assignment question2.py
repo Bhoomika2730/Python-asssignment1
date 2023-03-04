a6b4535b55d3bb372b4877e0bd4d50614b853dbf
Bhoomika2730 <125494668+Bhoomika2730@users.noreply.github.com>
2023-03-04 05:09:59 -1200

    Add files via upload

---
+++ Assignment question2.py
@@ -0,0 +1,14 @@
+#Write a Python program that accepts a word from the user and reverse it.
+#Sample Test Case
+#Input : Edyoda
+#output: adoydE
+
+word = input("Input a word to reverse: ")
+ 
+for char in range(len(word) - 1, -1, -1):
+  print(word[char], end="")
+print("\n")
+
+word = input("Enter the word to reverse:")
+word = "Edyoda"
+print(word[slice(-1,-7,-1)])
