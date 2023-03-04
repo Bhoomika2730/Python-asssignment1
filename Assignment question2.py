4dddd76d4541cd1fe732f2279e4a3f1f2831ca25
Bhoomika2730 <125494668+Bhoomika2730@users.noreply.github.com>
2023-03-04 05:23:57 -1200

    Update Assignment question2.py

--- Assignment question2.py
+++ Assignment question2.py
@@ -9,6 +9,8 @@ for char in range(len(word) - 1, -1, -1):
   print(word[char], end="")
 print("\n")
 
+# Another method
+
 word = input("Enter the word to reverse:")
 word = "Edyoda"
 print(word[slice(-1,-7,-1)])
